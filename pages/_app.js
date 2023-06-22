dad018e1be7f646f609458eb591016f00643b1af
Milan Pataki <milanpataki9@gmail.com>
2023-06-22 19:00:10 +0200

    auth context

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
 import Navbar from '@components/Navbar';
+import { UserContext } from '@lib/context';
 import '@styles/globals.css';
 import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
 
   return (
-    <>
+    <UserContext.Provider value={{ user: {}, username: 'jeff' }}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </UserContext.Provider>
   );
 }
 
