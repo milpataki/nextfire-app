ac55da6108f72cc671341c64b02f93b0b9d88714
Milan Pataki <milanpataki9@gmail.com>
2023-06-22 17:12:25 +0200

    simplified

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+
+
+import { useState } from 'react';
+
+
+
+export default function Home(props) {
+
+  return (
+    <main>
+      <div>
+        <h2>💡 Next.js + Firebase - The App</h2>
+      </div>
+    </main>
+  );
+}
