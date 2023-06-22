4f881a5aff5d149e4c38cd23eaedcef479b22e21
Milan Pataki <milanpataki9@gmail.com>
2023-06-22 20:30:05 +0200

    add auth hook

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,15 @@ import Navbar from '@components/Navbar';
 import { UserContext } from '@lib/context';
 import '@styles/globals.css';
 import { Toaster } from 'react-hot-toast';
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useEffect, useState } from 'react';
+import { useUserData } from '@lib/hooks';
 
 function MyApp({ Component, pageProps }) {
+  const userdata = useUserData();
 
   return (
-    <UserContext.Provider value={{ user: {}, username: 'jeff' }}>
+    <UserContext.Provider value={userdata}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
