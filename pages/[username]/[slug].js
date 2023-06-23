15707e29479a9af101cb4ed468b67cf1e476d124
Milan Pataki <milanpataki9@gmail.com>
2023-06-23 15:09:43 +0200

    ISR post editor

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,3 +1,47 @@
+import { getUserWithUsername } from "@lib/firebase";
+
+export async function getStaticProps({params})
+{
+  const {username, slug} = params;
+  const userDoc = await getUserWithUsername(username);
+
+  let post;
+  let path;
+
+  if (userDoc) {
+    const postRef = userDoc.ref.collection('posts').doc(slug);
+    post = postToJSON(await postRef.get());
+
+    path = postRef.path;
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 5000, // regenerate after this time
+  };
+}
+
+export async function getStaticPaths() {
+  // Improve my using Admin SDK to select empty docs
+  const snapshot = await firestore.collectionGroup('posts').get();
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: { username, slug },
+    };
+  });
+
+  return {
+    // must be in this format:
+    // paths: [
+    //   { params: { username, slug }}
+    // ],
+    paths,
+    fallback: 'blocking',
+  };
+}
+
 export default function PostPage({ }) {
   return (
     <main>
